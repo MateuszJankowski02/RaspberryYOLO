d3bd7f252de67361bbbff98a084507fdf46d3c5c
MateuszJankowski02 <twonightdragons@gmail.com>
2025-06-03 23:19:47 +0200

    Update model version in convert.py from YOLOv8n to YOLOv11n

--- backendYOLO/convert.py
+++ backendYOLO/convert.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-# Load a YOLOv8n PyTorch model
-model = YOLO("yolov8n.pt")
+# Load a YOLOv11n PyTorch model
+model = YOLO("yolov11n.pt")
 
 # Export the model to NCNN format
-model.export(format="ncnn")  # creates 'yolov8n_ncnn_model'
+model.export(format="ncnn")  # creates 'yolov11n_ncnn_model'
